2f0a1148ca2d6860c5b25c67c8b07309b47d4ac3
Aviroop Pal <aviroop123@yahoo.com>
2018-07-02 13:18:37 +0530

    Create SPOJ PROFIT.cpp
    Status : AC

---
+++ SPOJ/SPOJ PROFIT.cpp
@@ -0,0 +1,132 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef double f80;
+#define fi first
+#define se second
+#define pii pair<int,int>
+#define pb push_back
+const int mod = 1e9 + 7;
+#define int long long int
+
+typedef long long LL;
+
+struct Edge {
+  int from, to, cap, flow, index;
+  Edge(int from, int to, int cap, int flow, int index) :
+    from(from), to(to), cap(cap), flow(flow), index(index) {}
+};
+
+struct PushRelabel {
+  int N;
+  vector<vector<Edge> > G;
+  vector<LL> excess;
+  vector<int> dist, active, count;
+  queue<int> Q;
+
+  PushRelabel(int N) : N(N), G(N), excess(N), dist(N), active(N), count(2*N) {}
+
+  void AddEdge(int from, int to, int cap) {
+    G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+    if (from == to) G[from].back().index++;
+    G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1)); // for bidirectional set cap.
+  }
+
+  void Enqueue(int v) { 
+    if (!active[v] && excess[v] > 0) { active[v] = true; Q.push(v); } 
+  }
+
+  void Push(Edge &e) {
+    int amt = min(excess[e.from], LL(e.cap - e.flow));
+    if (dist[e.from] <= dist[e.to] || amt == 0) return;
+    e.flow += amt;
+    G[e.to][e.index].flow -= amt;
+    excess[e.to] += amt;    
+    excess[e.from] -= amt;
+    Enqueue(e.to);
+  }
+  
+  void Gap(int k) {
+    for (int v = 0; v < N; v++) {
+      if (dist[v] < k) continue;
+      count[dist[v]]--;
+      dist[v] = max(dist[v], N + 1);
+      count[dist[v]]++;
+      Enqueue(v);
+    }
+  }
+
+  void Relabel(int v) {
+    count[dist[v]]--;
+    dist[v] = 2*N;
+    for (int i = 0; i < G[v].size(); i++) 
+      if (G[v][i].cap - G[v][i].flow > 0)
+    dist[v] = min(dist[v], dist[G[v][i].to] + 1);
+    count[dist[v]]++;
+    Enqueue(v);
+  }
+
+  void Discharge(int v) {
+    for (int i = 0; excess[v] > 0 && i < G[v].size(); i++) Push(G[v][i]);
+    if (excess[v] > 0) {
+      if (count[dist[v]] == 1) 
+    Gap(dist[v]); 
+      else
+    Relabel(v);
+    }
+  }
+
+  LL GetMaxFlow(int s, int t) {
+    count[0] = N-1;
+    count[N] = 1;
+    dist[s] = N;
+    active[s] = active[t] = true;
+    for (int i = 0; i < G[s].size(); i++) {
+      excess[s] += G[s][i].cap;
+      Push(G[s][i]);
+    }
+    
+    while (!Q.empty()) {
+      int v = Q.front();
+      Q.pop();
+      active[v] = false;
+      Discharge(v);
+    }
+    
+    LL totflow = 0;
+    for (int i = 0; i < G[s].size(); i++) totflow += G[s][i].flow;
+    return totflow;
+  }
+};
+const int N = 5005;
+int val[N], p[N];
+signed main(){
+    //freopen("inp.txt", "r", stdin);
+    ios :: sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        for(int i = 1; i <= n; i++)
+            cin >> p[i];
+        fill(val + 1, val + n + 1, 0);
+        PushRelabel g(n + 2);
+        int tot = 0;
+        for(int i = 1; i <= m; i++){
+            int u, v, w;
+            cin >> u >> v >> w;
+            tot += w;
+            if(u > v)
+                swap(u, v);
+            g.AddEdge(u, v, w);
+            val[v] += w;
+        }
+        for(int i = 1; i <= n; i++){
+            g.AddEdge(0, i, p[i]);
+            g.AddEdge(i, n + 1, val[i]);
+        }
+        cout << tot - g.GetMaxFlow(0, n + 1) << endl;
+    }
+    return 0;
+}
