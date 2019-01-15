8f02891a3c3eca0a62780ba1a0a68581c5f73c8b
Aviroop Pal <aviroop123@yahoo.com>
2019-01-15 18:27:13 +0530

    Create CF-213171W.cpp

---
+++ CodeForces/CF-213171W.cpp
@@ -0,0 +1,180 @@
+#pragma GCC optimize ("Ofast")
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long int ll;
+#define int long long int
+#define pb push_back
+#define fi first
+#define se second
+#define deb cerr << "Line no." << __LINE__
+#define fr(i, a, b) for(int i = a; i <= b; i++)
+#define aint(x) x.begin(), x.end()
+#define IO ios :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define pii pair<int,int>
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+const int mod = 1e9 + 7;
+const int mod1 = 998244353;
+typedef double f80;
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+template<typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r){
+	uniform_int_distribution<int> uid(l, r);
+	return uid(rng);
+}
+int pwr(int a,int b){
+	int ans = 1;
+	while(b){
+		if(b & 1) ans = (ans * a) % mod;
+		a = (a * a) % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+struct base{
+    f80 x,y;
+    base(){x=y=0;}
+    base(f80 _x, f80 _y){x = _x,y = _y;}
+    base(f80 _x){x = _x, y = 0;}
+    void operator = (f80 _x){x = _x,y = 0;}
+    f80 real(){return x;}
+    f80 imag(){return y;}
+    base operator + (const base& b){return base(x+b.x,y+b.y);}
+    void operator += (const base& b){x+=b.x,y+=b.y;}
+    base operator * (const base& b){return base(x*b.x - y*b.y,x*b.y+y*b.x);}
+    void operator *= (const base& b){f80 p = x*b.x - y*b.y, q = x*b.y+y*b.x; x = p, y = q;}
+    void operator /= (f80 k){x/=k,y/=k;}
+    base operator - (const base& b){return base(x - b.x,y - b.y);}
+    void operator -= (const base& b){x -= b.x, y -= b.y;}
+    base conj(){ return base(x, -y);}
+    base operator / (f80 k) { return base(x / k, y / k);}
+    void Print(){ cerr << x <<  " + " << y << "i\n";}
+};
+const f80 PI = acos(-1);
+const int MAXN = 13;
+const int maxn = (1<<MAXN);
+base W[maxn],invW[maxn], P1[maxn], Q1[maxn];
+void precompute_powers(){
+    for(int i = 0;i<maxn/2;i++){
+        double ang = (2*PI*i)/maxn; 
+        f80 _cos = cos(ang), _sin = sin(ang);
+        W[i] = base(_cos,_sin);
+        invW[i] = base(_cos,-_sin);
+    }
+}
+void fft (vector<base> & a, bool invert) {
+    int n = (int) a.size();
+    for (int i=1, j=0; i<n; ++i) {
+        int bit = n >> 1;
+        for (; j>=bit; bit>>=1)
+            j -= bit;
+        j += bit;
+        if (i < j)
+            swap (a[i], a[j]);
+    }
+    for (int len=2; len<=n; len<<=1) {
+        for (int i=0; i<n; i+=len) {
+            int ind = 0,add = maxn/len;
+            for (int j=0; j<len/2; ++j) {
+                base u = a[i+j],  v = (a[i+j+len/2] * (invert?invW[ind]:W[ind]));
+                a[i+j] = (u + v);
+                a[i+j+len/2] = (u - v);
+                ind += add;
+            }
+        }
+    }
+    if (invert) for (int i=0; i<n; ++i) a[i] /= n;
+}
+
+// 4 FFTs in total for a precise convolution
+void mul_big_mod(vector<ll> &a, vector<ll> & b, ll mod){
+    int n1 = a.size(),n2 = b.size();
+    int final_size = a.size() + b.size() - 1;
+    int n = 1;
+    while(n < final_size) n <<= 1;
+    vector<base> P(n), Q(n);
+    int SQRTMOD = (int)sqrt(mod) + 10;
+    for(int i = 0;i < n1;i++) P[i] = base(a[i] % SQRTMOD, a[i] / SQRTMOD);
+    for(int i = 0;i < n2;i++) Q[i] = base(b[i] % SQRTMOD, b[i] / SQRTMOD);
+    fft(P, 0);
+    fft(Q, 0);
+    base A1, A2, B1, B2, X, Y;
+    for(int i = 0; i < n; i++){
+        X = P[i];
+        Y = P[(n - i) % n].conj();
+        A1 = (X + Y) * base(0.5, 0);
+        A2 = (X - Y) * base(0, -0.5);
+        X = Q[i];
+        Y = Q[(n - i) % n].conj();
+        B1 = (X + Y) * base(0.5, 0);
+        B2 = (X - Y) * base(0, -0.5);
+        P1[i] = A1 * B1 + A2 * B2 * base(0, 1);
+        Q1[i] = A1 * B2 + A2 * B1;
+    }
+    for(int i = 0; i < n; i++) P[i] = P1[i], Q[i] = Q1[i];
+    fft(P, 1);
+    fft(Q, 1);
+    a.resize(final_size);
+    for(int i = 0; i < final_size; i++){
+        ll x = (ll)(P[i].real() + 0.5);
+        ll y = (ll)(P[i].imag() + 0.5) % mod;
+        ll z = (ll)(Q[i].real() + 0.5);
+        a[i] = (x + ((y * SQRTMOD + z) % mod) * SQRTMOD) % mod;
+    }
+}
+void pow(vector<int> &v, int k){
+	int n = v.size();
+	vector<int> ans = {1};
+	while(k){
+		if(k & 1){
+			mul_big_mod(ans, v, mod);
+			ans.resize(n);
+		}
+		mul_big_mod(v, v, mod);
+		v.resize(n);
+		k >>= 1;
+	}
+	v = ans;
+}
+vector<int> v1;
+const int N = 2005;
+int fact[N];
+void solve(){
+	precompute_powers();
+	int n, k;
+	cin >> n >> k;
+	fact[0] = 1;
+	fr(i, 1, n){
+		fact[i] = (fact[i - 1] * i) % mod;
+	}
+	v1.resize(n + 1);
+	fr(i, 0, n){
+		v1[i] = pwr((fact[i] * fact[i]) % mod, mod - 2);
+	}
+	pow(v1, k);
+	cout << (v1[n] * ((fact[n] * fact[n]) % mod)) % mod;
+}
+signed main(){
+  	IO;
+  	#ifdef LOCAL
+		freopen("inp.txt","r", stdin);
+		// freopen("out.txt", "w", stdout);
+	#endif
+ 	clock_t clk = clock();
+  	int t = 1;
+  	// cin >> t;
+  	fr(tt, 1, t){
+    	solve();
+  	}
+  	cerr << endl << (double)(clock() - clk) / CLOCKS_PER_SEC;
+  	return 0;
+}
